295f021b54085e5d024b67574d19961bd4e0ab6c
Ai Kurihara <ai.coder.kurihara@gmail.com>
2026-04-10 16:20:09 +0100

    Initial Commit

---
+++ Floofbit/FloofbitApp.swift
@@ -0,0 +1,17 @@
+//
+//  FloofbitApp.swift
+//  Floofbit
+//
+//  Created by Ai Kurihara on 10/04/2026.
+//
+
+import SwiftUI
+
+@main
+struct FloofbitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
